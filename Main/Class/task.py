50f183da52b954289d8fa14631b950899a4faf4a
Khaled <Khaled.taha1918@gmail.com>
2025-01-23 01:22:02 +0200

    updated the class

---
+++ Main/Class/task.py
@@ -0,0 +1,22 @@
+class TASKS:
+
+    def __init__(self):
+        self.__title = None
+        self.__description = None
+        self.__date = None
+        self.__state = None
+
+    @property
+    def task(self):
+        return self.__title, self.__description, self.__date, self.__state
+
+    @task.setter
+    def task(self, task_details):
+        self.__title, self.__description, self.__date, self.__state = task_details
+
+    @task.deleter
+    def task(self):
+        del self.__title
+        del self.__description
+        del self.__date
+        del self.__state
