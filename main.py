aeb0893b5becfad15954d79de1e81fc4a50e4dcb
Khaled <Khaled.taha1918@gmail.com>
2025-01-23 01:54:57 +0200

    update main and arrangment

---
+++ main.py
@@ -0,0 +1,26 @@
+from Database import data
+from Class import task
+
+
+def add_task():
+    pass
+
+
+def delete_task():
+    pass
+
+
+def edit_task():
+    pass
+
+
+def search():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
